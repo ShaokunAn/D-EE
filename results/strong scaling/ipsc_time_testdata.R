a6052ed664998e0b21d809bfecbdfec03b18d8a0
ShaokunAn <skan_seu@163.com>
2020-10-10 18:53:43 +0800

    update generation process of test data

--- results/strong scaling/ipsc_time_testdata.R
+++ results/strong scaling/ipsc_time_testdata.R
@@ -1,9 +1,9 @@
 # generate test data to obtain the time consumption of various numbers of data  
 setwd('YourPath/D-EE/results/strong scaling')
-ipsc_afterpca_all <- read.csv('ipsc_afterpca.csv', header = FALSE)
-ipsc_afterpca_1w <- ipsc_afterpca_all[1:10000, ]
-ipsc_afterpca_5w <- ipsc_afterpca_all[1:50000,]
-ipsc_afterpca_10w <- ipsc_afterpca_all[1:100000,]
+ipsc_afterpca_10w <- read.csv('ipsc_afterpca_10w.csv', header = FALSE)
+ipsc_afterpca_5w <- ipsc_afterpca_10w[1:50000,]
+ipsc_afterpca_1w <- ipsc_afterpca_10w[10000,]
+
 write.table(ipsc_afterpca_1w, file = 'ipsc_afterpca_1w.csv', 
             col.names = FALSE, row.names = FALSE, sep = ',')
 write.table(ipsc_afterpca_5w, file = 'ipsc_afterpca_5w.csv', 
